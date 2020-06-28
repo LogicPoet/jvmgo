e851d85cc5166fa4fec4a3b84e2b06dcf6e0c67f
Liuzhi <zhi.liu@maiunsoft.com>
2020-06-28 15:46:35 +0800

    简单的java命令行工具

---
+++ jvmgo/ch01/cmd.go
@@ -0,0 +1,39 @@
+// 定义一个结构体来表示命令行选项和参数
+// 在go语言中，main是一个特殊的包，这个包所在的目录（可以叫作任何名字）会被编译为可执行文件
+// go程序的入口也是main()函数，但是不接收任何参数，也不能有返回值
+package main 
+import "flag"
+import "fmt"
+import "os"
+type Cmd struct {
+	helpFlag	bool
+	versionFlag	bool
+	cpOption	string
+	class	string
+	args []string
+}
+
+/**
+ * Go语言有函数（Function）和方法（Method）之分，方法调用需要
+ * receiver，函数调用则不需要。
+ */
+func parseCmd() *Cmd {
+	cmd := &Cmd{}
+	flag.Usage = printUsage
+	flag.BoolVar(&cmd.helpFlag, "help", false, "print help message")
+	flag.BoolVar(&cmd.helpFlag, "?", false, "print help message")
+	flag.BoolVar(&cmd.versionFlag, "version", false, "print version and exit")
+	flag.StringVar(&cmd.cpOption, "classpath", "", "classpath")
+	flag.StringVar(&cmd.cpOption, "cp" ,"", "classpath")
+	flag.Parse()
+	args := flag.Args()
+	if len(args) > 0 {
+		cmd.class = args[0]
+		cmd.args = args[1:]
+	}
+	return cmd
+}
+
+func printUsage(){
+	fmt.Printf("Usage: %s [-options] class [args...]\n", os.Args[0])
+}
